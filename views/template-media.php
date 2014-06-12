7521c680be831167ee889ab46fd8679dcac18bcb
james2doyle <james2doyle@gmail.com>
2014-06-12 19:44:11 -0400

    clear everything!

--- views/template-media.php
+++
@@ -1,7 +0,0 @@
-<?php foreach($images as $image): ?>
-  <div class="upload-thumb">
-    <a href="<?php echo $image['src'] ?>" target="_blank" title="<?php echo $image['title'] ?>"><img src="<?php echo $image['src'] ?>" width="<?php echo $image['width'] ?>" height="<?php echo $image['height'] ?>" alt="<?php echo $image['alt'] ?>"></a>
-    <p><?php echo $image['filename'] ?></p>
-    <a href="#" class="btn red top-close">X</a>
-  </div>
-<?php endforeach; ?>
